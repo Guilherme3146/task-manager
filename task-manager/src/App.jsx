ebfda8f942f250772ec70df8d8bbf3b5670f3d7e
Guilherme. <guilherme.melo4631@gmail.com>
2026-05-07 20:28:50 -0300

    feat: Sidebar and Sidebar components

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css"
+import Sidebar from "./components/Sidebar"
 
 function App() {
   return (
-    <>
-      <h1 className="mb-4 pt-3 text-2xl text-red-500">Olá</h1>
-    </>
+    <div>
+      <Sidebar />
+    </div>
   )
 }
 
