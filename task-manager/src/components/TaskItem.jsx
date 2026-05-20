f773e4b187d5ee6ce5eb37faaf9b8d5f6beab820
Guilherme. <guilherme.melo4631@gmail.com>
2026-05-19 22:11:27 -0300

    feat: Task Item and change status

---
+++ task-manager/src/components/TaskItem.jsx
@@ -0,0 +1,47 @@
+import CheckIcon from "../assets/icons/check.svg?react"
+import LoaderIcon from "../assets/icons/loader.svg?react"
+import DetailIcon from "../assets/icons/details.svg?react"
+
+const TaskItem = ({ task }) => {
+  const getStatusClass = () => {
+    switch (task.status) {
+      case "in_progress":
+        return "bg-[#FFAA04] text-[#FFAA04]"
+      case "done":
+        return "bg-[#00adb5] text-[#00adb5]"
+      case "not_started":
+        return "bg-[#35383E] bg-current/10 text-[#35383E]"
+      default:
+        return "bg-[#35383E] bg-current/10 text-[#35383E]"
+    }
+  }
+
+  return (
+    <div
+      className={`flex items-center justify-between gap-2 rounded-lg bg-current/10 px-4 py-3 text-sm transition ${getStatusClass()}`}
+    >
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClass()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === "done"}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === "done" && <CheckIcon />}
+          {task.status === "in_progress" && (
+            <LoaderIcon className="animate-spin" />
+          )}
+        </label>
+
+        {task.title}
+      </div>
+
+      <a href="#" className="hover:opacity-75">
+        <DetailIcon />
+      </a>
+    </div>
+  )
+}
+export default TaskItem
---
+++ task-manager/src/constants/tasks.js
@@ -0,0 +1,46 @@
+const TASKS = [
+  {
+    id: 1,
+    title: "Comprar pão",
+    description: "Comprar pão na padaria",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 2,
+    title: "Estudar React",
+    description: "Estudar React para o projeto",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 3,
+    title: "Fazer exercícios",
+    description: "Fazer exercícios para manter a saúde",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 4,
+    title: "Ler um livro",
+    description: "Ler um livro para relaxar",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 5,
+    title: "Assistir série",
+    description: "Assistir série para se divertir",
+    time: "night",
+    status: "not1_started",
+  },
+  {
+    id: 6,
+    title: "Dormir cedo",
+    description: "Dormir cedo para descansar",
+    time: "night",
+    status: "not_started",
+  },
+]
+
+export default TASKS
